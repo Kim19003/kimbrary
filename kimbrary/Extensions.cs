80375f88ff4a18825ba7d1e4d9dae9a4ee0fa766
Kim Järvinen <kim.jarvinen@student.lab.fi>
2022-05-30 23:23:52 +0300

    Update Extensions.cs

--- kimbrary/Extensions.cs
+++ kimbrary/Extensions.cs
@@ -1,9 +1,22 @@
+using System.Security.Cryptography;
+
 namespace Kimbrary
 {
     namespace Extensions
     {
         public static class Extensions
         {
+			public static int Next(this RandomNumberGenerator randomNumberGenerator, int minValue, int maxValue)
+            {
+                maxValue -= 1;
+
+                byte[] randomBytes = new byte[sizeof(int)];
+                randomNumberGenerator.GetNonZeroBytes(randomBytes);
+                int randomInt = BitConverter.ToInt32(randomBytes);
+
+                return ((randomInt - minValue) % (maxValue - minValue + 1) + (maxValue - minValue + 1)) % (maxValue - minValue + 1) + minValue;
+            }
+			
             public static int IndexOfAny(this string value, params string[] items)
             {
                 foreach (var item in items)
